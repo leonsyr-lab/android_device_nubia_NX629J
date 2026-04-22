89d9e35e20d5756c23d4117f6518dbed58bba5c6
leonsyr-lab <leonsyr-lab@users.noreply.github.com>
2026-04-23 00:52:31 +0300

    Fix build: remove AOSP-built files from PRODUCT_COPY_FILES
    LESSON LEARNED: Never use PRODUCT_COPY_FILES for files that the
    AOSP build system already knows how to build. This includes:
    - HIDL interface stubs (android.hardware.*@version.so)
    - Keymaster support libraries (libkeymaster4support.so, etc.)
    - Gatekeeper libraries (libgatekeeper.so, etc.)
    - System binaries (gatekeeperd, vold, vold_prepare_subirs)
    
    These cause 'overriding commands' fatal errors during build.
    Only vendor-proprietary blobs should be in PRODUCT_COPY_FILES.
    
    The AOSP build + qcom common tree's relink step will provide
    all standard system libraries automatically.

--- twrp_NX629J.mk
+++ twrp_NX629J.mk
@@ -2,6 +2,11 @@
 # TWRP for Nubia Red Magic 3 (NX629J) - Android 11 CN ROM
 # FBE + ICE decryption with qcom common tree
 #
+# LESSON LEARNED: Do NOT use PRODUCT_COPY_FILES for any file that the
+# AOSP build system already builds (HIDL stubs, keymaster support libs,
+# gatekeeper libs, vold, etc.). Only use it for vendor-proprietary blobs.
+# Overlapping targets cause "overriding commands" build failures.
+#
 
 # Release name
 PRODUCT_RELEASE_NAME := NX629J
@@ -10,22 +15,30 @@ PRODUCT_RELEASE_NAME := NX629J
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Qualcomm FBE decryption packages (from qcom common tree)
+# These handle: init.rc imports, prepdecrypt.sh, service startup, relink
 PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
-# Copy vendor crypto blobs into recovery ramdisk /system/bin/ (TWRP convention for Android 10+)
+# Copy ONLY vendor-proprietary blobs that are NOT built by AOSP.
+# Service binaries (QTI-specific, not in AOSP source):
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/bin/hw/android.hardware.keymaster@4.1-service-qti:recovery/root/system/bin/android.hardware.keymaster@4.1-service-qti \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/bin/hw/android.hardware.keymaster@4.0-service-qti:recovery/root/system/bin/android.hardware.keymaster@4.0-service-qti \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/bin/hw/android.hardware.keymaster@3.0-service-qti:recovery/root/system/bin/android.hardware.keymaster@3.0-service-qti \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/bin/hw/android.hardware.gatekeeper@1.0-service-qti:recovery/root/system/bin/android.hardware.gatekeeper@1.0-service-qti \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/bin/hw/vendor.qti.hardware.qseecom@1.0-service:recovery/root/system/bin/vendor.qti.hardware.qseecom@1.0-service \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/bin/hw/vendor.qti.hardware.cryptfshw@1.0-service-qti:recovery/root/system/bin/vendor.qti.hardware.cryptfshw@1.0-service-qti \
+    $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/bin/hw/vendor.qti.hardware.cryptfshw@1.0-service-qti:recovery/root/system/bin/vendor.qti.hardware.cryptfshw@1.0-service-qti
+
+# Vendor implementation .so files (QTI-specific, not in AOSP):
+PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/hw/android.hardware.keymaster@3.0-impl-qti.so:recovery/root/vendor/lib64/hw/android.hardware.keymaster@3.0-impl-qti.so \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so:recovery/root/vendor/lib64/hw/android.hardware.gatekeeper@1.0-impl-qti.so \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/hw/vendor.qti.hardware.qseecom@1.0-impl.so:recovery/root/vendor/lib64/hw/vendor.qti.hardware.qseecom@1.0-impl.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/hw/vendor.qti.hardware.qteeconnector@1.0-impl.so:recovery/root/vendor/lib64/hw/vendor.qti.hardware.qteeconnector@1.0-impl.so \
+    $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/hw/vendor.qti.hardware.qteeconnector@1.0-impl.so:recovery/root/vendor/lib64/hw/vendor.qti.hardware.qteeconnector@1.0-impl.so
+
+# Vendor-specific .so files (definitely not in AOSP):
+PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/libQSEEComAPI.so:recovery/root/vendor/lib64/libQSEEComAPI.so \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/libQTEEConnector_vendor.so:recovery/root/vendor/lib64/libQTEEConnector_vendor.so \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/libqtikeymaster4.so:recovery/root/vendor/lib64/libqtikeymaster4.so \
@@ -37,25 +50,9 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/libcryptfshwhidl.so:recovery/root/vendor/lib64/libcryptfshwhidl.so \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/vendor.qti.hardware.cryptfshw@1.0.so:recovery/root/vendor/lib64/vendor.qti.hardware.cryptfshw@1.0.so \
     $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/libGPQTEEC_vendor.so:recovery/root/vendor/lib64/libGPQTEEC_vendor.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/libGPTEE_vendor.so:recovery/root/vendor/lib64/libGPTEE_vendor.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/android.hardware.keymaster@4.1.so:recovery/root/system/lib64/android.hardware.keymaster@4.1.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/android.hardware.keymaster@4.0.so:recovery/root/system/lib64/android.hardware.keymaster@4.0.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/android.hardware.keymaster@3.0.so:recovery/root/system/lib64/android.hardware.keymaster@3.0.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/android.hardware.gatekeeper@1.0.so:recovery/root/system/lib64/android.hardware.gatekeeper@1.0.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/libkeymaster4_1support.so:recovery/root/system/lib64/libkeymaster4_1support.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/libkeymaster4support.so:recovery/root/system/lib64/libkeymaster4support.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/libkeymaster_messages.so:recovery/root/system/lib64/libkeymaster_messages.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/libkeymaster_portable.so:recovery/root/system/lib64/libkeymaster_portable.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/libsoftkeymasterdevice.so:recovery/root/system/lib64/libsoftkeymasterdevice.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/libpuresoftkeymasterdevice.so:recovery/root/system/lib64/libpuresoftkeymasterdevice.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/libgatekeeper.so:recovery/root/system/lib64/libgatekeeper.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/lib64/libgatekeeper_aidl.so:recovery/root/system/lib64/libgatekeeper_aidl.so \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/bin/gatekeeperd:recovery/root/system/bin/gatekeeperd \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/bin/wait_for_keymaster:recovery/root/system/bin/wait_for_keymaster \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/bin/vold:recovery/root/system/bin/vold \
-    $(DEVICE_PATH)/vendor/nubia/NX629J/system/bin/vold_prepare_subdirs:recovery/root/system/bin/vold_prepare_subdirs
+    $(DEVICE_PATH)/vendor/nubia/NX629J/vendor/lib64/libGPTEE_vendor.so:recovery/root/vendor/lib64/libGPTEE_vendor.so
 
-## Device identifier
+# Device identifier
 PRODUCT_DEVICE := NX629J
 PRODUCT_NAME := twrp_NX629J
 PRODUCT_BRAND := nubia
